15ba38a66dab7381e1020a42b087d3222190d228
Watanabe Iwao <iwaowatanabe@gmail.com>
2018-08-02 07:29:33 +0900

    お試し実行のコード追加

--- hitk/__main__.py
+++ hitk/__main__.py
@@ -1,2 +1,17 @@
-import hitk
-print('hello')
+import sys
+from hitk import hello, memo01
+
+Apps = [
+    hello.HelloApp,
+    memo01.MemoApp,
+]
+
+args = list(sys.argv)
+args.pop(0)
+
+num = int(args[0]) if args else 0
+
+Apps[num].run(args)
+
+
+
