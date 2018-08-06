4ddaf0105b428e1851777ef63b871a5a95a9a9a4
Watanabe Iwao <iwaowatanabe@gmail.com>
2018-08-07 07:33:07 +0900

    idle向けの設定を追加

--- setup.py
+++ setup.py
@@ -7,6 +7,9 @@ from setuptools import setup
 requirements = [
 ]
 
+# IDLEで動作させるときには以下のコメントを外して実行します
+# for tt in ('develop', '--user'): sys.argv.append(tt)
+
 setup(name='hitk',
       version='0.1',
       description=u'Tkinterを利用するpythonのサンプルコードが含まれます',
@@ -17,3 +20,7 @@ setup(name='hitk',
       install_requires=requirements,
       zip_safe=True,
      )
+
+# python 3.6 には setuptools が含まれないことがあるので、次の要領で導入します
+# curl https://bootstrap.pypa.io/ez_setup.py | python3 - --user
+
